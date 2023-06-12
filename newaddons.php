328101ca0e950aca6bca374d5d8b13bf94026bfa
arkaprrava-wisdmlabs <arkaprava.chakraborty@wisdmlabs.com>
2023-06-12 14:32:20 +0530

    defined the plugin

---
+++ newaddons.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Plugin Name: New Addons
+ * Description: Add ons created for additional functionality of WooCommerce.
+ * Version: 1.0.0
+ * Author: Arkaprava
+ * Text Domain: na
+ * Requires at least: 6.0
+ * Requires PHP: 7.3
+ *
+ * @package WooCommerce
+ */
